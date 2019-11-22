41ed251ed154085392763f199ac6b065c218edc2
Laura Campa <lauras_here@pm.me>
2019-11-22 13:00:46 -0800

    Add testimonial, modify contact form

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,31 @@
-const menuToggle = document.querySelector('#menu-toggler');
-const html = document.querySelector('html');
-const menuContent = document.querySelector('.nav__content');
-const navBar = document.querySelector('.navbar');
-const menuToggleIcon = menuToggle.children;
+var menuToggle = document.querySelector('#menu-toggler');
+var html = document.querySelector('html');
+var menuContent = document.querySelector('.nav__content');
+var navBar = document.querySelector('.navbar');
+var menuToggleIcon = menuToggle.children;
 
 document.addEventListener('DOMContentLoaded', () => {
     html.classList.toggle('js');
 });
 
-menuToggle.addEventListener('click', () => {
-   menuContent.classList.toggle('show'); 
-   if (menuContent.classList.contains('show')) {
-       menuToggleIcon.item(0).innerHTML = 'Close';
-       menuToggleIcon.item(1).classList.replace('fa-bars', 'fa-window-close');
-   } else {
-       menuToggleIcon.item(0).innerHTML = 'Menu';
-       menuToggleIcon.item(1).classList.replace('fa-window-close', 'fa-bars');
-   }
+menuToggle.addEventListener('click', function() {
+    menuContent.classList.toggle('show'); 
+    if (menuContent.classList.contains('show')) {
+        menuToggleIcon.item(0).innerHTML = 'Close';
+        menuToggleIcon.item(1).classList.replace('fa-bars', 'fa-window-close');
+    } else {
+        menuToggleIcon.item(0).innerHTML = 'Menu';
+        menuToggleIcon.item(1).classList.replace('fa-window-close', 'fa-bars');
+    }
 });
 
-formCheckbox = document.querySelector('#terms');
-formSubmit = document.querySelector('#submit');
+/*
+var formCheckbox = document.querySelector('#terms');
+var formSubmit = document.querySelector('#submit');
 
-formCheckbox.onchange = function() {
-    formSubmit.disabled = !this.checked;
+if (formCheckbox) {
+    formCheckbox.onchange = function() {
+        formSubmit.disabled = !this.checked;
+    }
 };
+*/
